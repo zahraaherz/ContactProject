64496a5360d317ac4e147ad5860010a5adae73a9
zahraaherz <61514777+zahraaherz@users.noreply.github.com>
2022-06-10 22:44:38 +0300

    Add files via upload

---
+++ Contacta/Contacta/AddContact.swift
@@ -0,0 +1,289 @@
+//
+//  AddContact.swift
+//  Contacta
+//
+//  Created by Zahraa Herz on 09/06/2022.
+//
+
+import UIKit
+
+class AddContact: UIViewController {
+
+    //Mark: Properties
+
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var firstName: UITextField!
+    
+    @IBOutlet var lastName: UITextField!
+        
+    @IBOutlet var phoneNumber: UITextField!
+    
+    @IBOutlet var email: UITextField!
+    
+    @IBOutlet var firstNameValidation: UILabel!
+    
+    @IBOutlet var lastNameValidation: UILabel!
+    
+    @IBOutlet var emailValidation: UILabel!
+    
+    @IBOutlet var phoneValidation: UILabel!
+    
+    @IBOutlet var addButton: UIButton!
+    
+    
+    var isFirstNameValid = false
+    
+    var isLastNameValid = false
+    
+    var isEmailValid = false
+    
+    var isPhoneValid = false
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var lastNameStr = ""
+    
+    var firstNameStr = ""
+
+
+    //MARK: -  Action Buttons
+    
+    @IBAction func firstNameChanged(_ sender: Any) {
+        
+        // Validation
+
+        let firstNameString = firstName.text ?? ""
+        
+        if validateString(string: firstNameString){
+            
+            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.green]
+            
+            firstNameValidation.attributedText = NSAttributedString(string: "Valid", attributes: attributes)
+            
+            isFirstNameValid = true
+                    
+        } else {
+            
+            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
+            
+            firstNameValidation.attributedText = NSAttributedString(string: "Invalid", attributes: attributes)
+            
+            isFirstNameValid = false
+        }
+        
+        // Image Chenged
+        ChangeImage()
+        
+    }
+    
+    @IBAction func lastNameChanged(_ sender: Any) {
+        
+        
+        // Validation
+        let lastNameString = lastName.text ?? ""
+        
+        if validateString(string: lastNameString){
+            
+            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.green]
+            
+            lastNameValidation.attributedText = NSAttributedString(string: "Valid", attributes: attributes)
+            
+            isLastNameValid = true
+                    
+        } else {
+            
+            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
+            
+            lastNameValidation.attributedText = NSAttributedString(string: "Invalid", attributes: attributes)
+            
+            isLastNameValid = false
+        }
+        
+        // Image Chenged
+        ChangeImage()
+
+    }
+    
+    
+    @IBAction func phoneChanged(_ sender: Any) {
+        
+        let phone = phoneNumber.text ?? ""
+        
+        validatePhoneNumber(phone)
+    
+    }
+    
+    
+    @IBAction func emailChanged(_ sender: Any) {
+        
+        let email = email.text ?? "" 
+        
+        validateEmail(email: email)
+    }
+    
+    
+    @IBAction func addAction(_ sender: Any) {
+        
+        if isLastNameValid && isFirstNameValid && isEmailValid && isPhoneValid {
+            
+            // Create a contact object
+            let newContact = Contacts(context: context )
+            newContact.firstName = firstName.text
+            newContact.lastName = lastName.text
+            newContact.email = email.text
+            newContact.phoneNumber = phoneNumber.text
+
+            // Save the data
+            do {
+                try self.context.save()
+            } catch {
+                  let nserror = error as NSError
+                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+            
+            // Go back to view controller
+            self.navigationController?.popViewController(animated: true)
+
+        } else {
+            
+            // Create new Alert
+            let dialogMessage = UIAlertController(title: "Error", message: "Make sure that every faield is valid", preferredStyle: .alert)
+            
+            // Create OK button with action handler
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                print("Ok button tapped")
+             })
+            
+            //Add OK button to a dialog message
+            dialogMessage.addAction(ok)
+
+            self.present(dialogMessage, animated: true, completion: nil)
+            
+        }
+    }
+    
+    //MARK: -  Other Methods
+    
+    func validatePhoneNumber(_ value: String){
+        
+        let set = CharacterSet(charactersIn: value)
+        
+        if !CharacterSet.decimalDigits.isSuperset(of: set) {
+            
+            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
+
+            phoneValidation.attributedText = NSAttributedString(string: "Phone number should contain only digit", attributes: attributes)
+            
+            isPhoneValid = false
+            
+        } else if value.count != 8 {
+            
+            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
+
+            phoneValidation.attributedText = NSAttributedString(string: "Phone number sould be 8 digits", attributes: attributes)
+            
+            isPhoneValid = false
+            
+        } else {
+            
+            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.green]
+
+            phoneValidation.attributedText = NSAttributedString(string: "Valid number ", attributes: attributes)
+            
+            isPhoneValid = true
+            
+        }
+    }
+    
+    
+    func validateString(string : String ) -> Bool {
+        
+        let predicateTest = NSPredicate(format: "SELF MATCHES %@", "[a-zA-Z]{3,22}")
+                
+        if predicateTest.evaluate(with: string) {
+                
+            return true
+                
+        } else {
+            
+            return false
+            
+        }
+    }
+    
+    
+    func validateEmail(email : String){
+        
+        let regularExpression = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression)
+        
+        if predicate.evaluate(with: email){
+            
+            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.green]
+
+            emailValidation.attributedText = NSAttributedString(string: "Valid Email", attributes: attributes)
+            
+            isEmailValid = true
+            
+        } else {
+            
+            let attributes = [NSAttributedString.Key.foregroundColor: UIColor.red]
+
+            emailValidation.attributedText = NSAttributedString(string: "Invalid Email", attributes: attributes)
+            
+            isEmailValid = false
+        }
+        
+    }
+
+    
+    func ChangeImage(){
+                
+        
+        if lastName.text!.isEmpty && firstName.text!.isEmpty {
+            
+            imageView.image = UIImage(named: "Profile")
+            
+        } else {
+            
+            // Cheking if the last name is empty or not
+            if !lastName.text!.isEmpty  {
+                
+                lastNameStr =  String(lastName.text!.first!)
+             
+            } else {
+                
+                lastNameStr = ""
+            }
+            
+            // Cheking if the first name is empty or not
+            if !firstName.text!.isEmpty {
+                
+                firstNameStr = String(firstName.text!.first!)
+            }else {
+                
+                firstNameStr = ""
+            }
+            
+            // Create a label to view the first letter of the user first name and last name
+            let imageLabel = UILabel()
+            imageLabel.frame.size = CGSize(width: 100.0, height: 100.0)
+            imageLabel.textColor = UIColor.white
+            imageLabel.text = firstNameStr.uppercased() + lastNameStr.uppercased()
+            imageLabel.font = imageLabel.font.withSize(30)
+            imageLabel.textAlignment = NSTextAlignment.center
+            imageLabel.backgroundColor = UIColor.blue
+            imageLabel.layer.masksToBounds = true
+            imageLabel.layer.cornerRadius = 50.0
+            
+            UIGraphicsBeginImageContext(imageLabel.frame.size)
+            imageLabel.layer.render(in: UIGraphicsGetCurrentContext()!)
+            imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            
+        }
+
+    }
+}
---
+++ Contacta/Contacta/ViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  Contacta
+//
+//  Created by Zahraa Herz on 09/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //Mark: Properties
+    
+    @IBOutlet var searchBar: UISearchBar!
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var contacts: [Contacts]?
+    
+    var filteredData: [Contacts]!
+    
+    var isFiltered = false
+
+    //Mark: View Life cycle Methods
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        fetchContacts()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+
+        fetchContacts()
+        
+    }
+    
+    // MARK: - Other Methods
+    
+    func fetchContacts(){
+        
+    do {
+        self.contacts = try context.fetch(Contacts.fetchRequest())
+
+        DispatchQueue.main.async {
+                
+            self.tableView.reloadData()
+            
+            }
+        
+        } catch {
+            
+            let nserror = error as NSError
+              fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+
+}
+
+// MARK: - Search bar data source
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        
+        searchBar.resignFirstResponder()
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar){
+        
+        searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar){
+        
+        searchBar.resignFirstResponder()
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        isFiltered = false
+        tableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        
+        if searchText.count == 0{
+            
+            isFiltered = false
+            
+        } else {
+            
+            isFiltered = true
+            filteredData = searchText.isEmpty ? contacts : contacts!.filter({
+                (dataString: Contacts) -> Bool in
+                return (((dataString.firstName! + " " + dataString.lastName!).range(of: searchText,options: .caseInsensitive) != nil) )
+            })
+        }
+
+        tableView.reloadData()
+    }
+}
+
+// MARK: - Table view data source
+
+extension ViewController: UITableViewDataSource , UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if !isFiltered {
+            
+            return self.contacts?.count ?? 0
+            
+        } else {
+            
+            return filteredData.count
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let name = isFiltered ? filteredData![indexPath.row] : contacts![indexPath.row]
+                
+        cell.textLabel?.text = name.firstName! + " " + name.lastName!
+        
+        return cell
+    }
+    
+}
